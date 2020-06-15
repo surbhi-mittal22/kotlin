ba16a0b30abd68eabcbe371fe881377816bb9b30
Surbhi Mittal <65413535+surbhi-mittal22@users.noreply.github.com>
2020-06-15 20:16:30 +0530

    Update Robot.kt

--- projectRobot/Robot.kt
+++ projectRobot/Robot.kt
@@ -1,52 +1,56 @@
-class Robot {
-    fun ringAlarm(time:Int)
-    {
-        println("Ring the alrm at specified time")
+import java.util.*
+
+/*Class Robot defines the tasks which will be performed by the Robot
+* You can customize this class according to your preferences*/
+class Robot(val name: String) {
+
+    fun ringAlarm(time: String) = println("Good morning!! Its $name, The time is already $time, please wake up!!!")
+
+    fun makeCoffee(coffee: Coffee) = if (coffee.black) {
+        println("Your black coffee with ${coffee.spoonsOfSugar} spoons of sugar is ready!")
+    } else {
+        println("Your coffee with ${coffee.spoonsOfSugar} spoons of sugar is ready!")
     }
 
-    fun makeCoffee(coffee:String,sugar:Int)
-    {
-        println("Make ${coffee} coffee everyday with $sugar tablespoon of sugar")
+    fun heatWater(temperature: Int) = println("Bath water with $temperature is ready, take a bath now!")
+
+    fun packBag(day: Int) {
+        val messageTemplate = "Books kept for today are : "
+        val subjects =
+            arrayOf("Maths", "Physics", "Chemistry", "Electronics", "Computers", "Material Sciences", "Human Values")
+
+        when (day) {
+            Calendar.MONDAY -> println("$messageTemplate ${subjects[0]}, ${subjects[1]}, ${subjects[2]}")
+            Calendar.TUESDAY -> println("$messageTemplate ${subjects[0]}, ${subjects[1]}, ${subjects[2]}")
+            Calendar.WEDNESDAY -> println("$messageTemplate ${subjects[3]}, ${subjects[4]}")
+            Calendar.THURSDAY -> println("$messageTemplate ${subjects[3]}, ${subjects[4]}")
+            Calendar.FRIDAY -> println("$messageTemplate ${subjects[5]}, ${subjects[6]}")
+            else -> println("Invalid day or maybe its holiday today!!")
+        }
     }
 
-    fun heatWater(temp:Double)
-    {
-        println("heat the water at $temp degree  for bath")
+    /*Here a random choice is made by the robot using the system defined method random()*/
+    fun cook() {
+        val breakfast = mutableListOf("Cereals", "Cornflakes", "Oats", "Sandwiches", "Eggs")
+        val lunch =
+            mutableListOf("Corn Spinach", "Salad", "Chicken", "Shahi Paneer", "Mashed Potatoes", "Eat out today!")
+        println("Today's breakfast is ${breakfast.random()} and lunch is ${lunch.random()}")
     }
 
-       val timetable = listOf<String>("Android","JAVA","Javascript","compiler","AI")
-    fun packBag(day:String)
-    {
-        when(day)
-        {
-            "monday" -> println("${day} timetable set ${timetable[0]}")
-            "tuesday" -> println("${day} timetable set ${timetable[1]}")
-            "wednesday" -> println("${day} timetable set ${timetable[2]}")
-            "thrusday"  ->  println("${day} timetable set ${timetable[3]}")
-            "friday" -> println("${day} timetable set ${timetable[4]}")
+    fun ironClothes(shirtColor: String, trouserColor: String) {
+        val shirtColors = mutableListOf("Blue", "Green", "Orange", "Red", "Yellow", "Black")
+        val trouserColors = mutableListOf("Blue", "Black", "Gray")
+
+        if (shirtColors.contains(shirtColor)) {
+            if (trouserColors.contains(trouserColor)) {
+                println("Your Clothes are ready!!")
+            } else {
+                println("You do not have trouser of this color. May be its time to go for a trouser shopping.")
+            }
+        } else {
+            println("You do not have shirt of this color. May be its time to go for a trouser shopping.")
         }
     }
 
 
-    var breakFast = listOf("Bread","Fruits","Oats","Milkshake")
-    var lunch = listOf("Pulao","Chicken Tikka","Cobbler","black pudding")
-
-
-    fun cookFood(shift : String)
-    {
-        if(shift == "breakfast")
-        {
-            println(breakFast.random())
-        }
-        else
-        {
-            println(lunch.random())
-        }
-
-    }
-
-    fun ironClothes()
-    {
-        println("iron the clothes")
-    }
 }
